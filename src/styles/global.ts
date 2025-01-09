9aecf4dfc59aae69e3878ec168b641be12e5b02d
youtheyeon <yuseoyeon@ewhain.net>
2025-01-09 13:57:18 +0900

    init: global style 수정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,35 +1,26 @@
-import { createGlobalStyle } from "styled-components";
-import Reset from "./reset";
-import Font from "./font";
+import { createGlobalStyle } from 'styled-components';
+import Reset from './reset';
+import Font from './font';
 
 const GlobalStyle = createGlobalStyle`
   ${Reset}
   ${Font}
 
+  * {
+    box-sizing: border-box;
+  }
+
   html,
   body {
+    width: 100%;
+    min-height: 100dvh;
     background: ${({ theme }) => theme.colors.grey03};
     color: ${({ theme }) => theme.colors.black};
     font-family: "Noto Sans KR", "Bebas Neue", sans-serif;
     font-size: 62.5%;
-    transition: background 0.3s ease;
-
+    
     -ms-overflow-style: none; 
     scrollbar-width: none; 
-
-    &::-webkit-scrollbar {
-      display: none;
-      width: 0;  
-      height: 0;
-      background: transparent;  
-      -webkit-appearance: none;
-    }
-  }
-
-  * {
-    box-sizing: border-box;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
 
     &::-webkit-scrollbar {
       display: none;
